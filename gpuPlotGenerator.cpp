d1a3e81ff1c00e678627a0f680f112bb0f9c19ae
cryo <bhamon.cryo@gmail.com>
2018-01-08 09:18:56 +0100

    Version number upgraded to `4.1.4`. Changelog updated.

--- gpuPlotGenerator.cpp
+++ gpuPlotGenerator.cpp
@@ -22,7 +22,7 @@
 
 int main(int p_argc, char* p_argv[]) {
 	std::cout << "-------------------------" << std::endl;
-	std::cout << "GPU plot generator v4.1.3" << std::endl;
+	std::cout << "GPU plot generator v4.1.4" << std::endl;
 	std::cout << "-------------------------" << std::endl;
 	std::cout << "Author:   Cryo" << std::endl;
 	std::cout << "Bitcoin:  138gMBhCrNkbaiTCmUhP9HLU9xwn5QKZgD" << std::endl;
